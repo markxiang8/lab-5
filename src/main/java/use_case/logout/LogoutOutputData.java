a55e0ba21826ef78e37b85d41a81e92baa2009c3
Zife1Luo <228phyllis@gmail.com>
2024-10-22 23:39:19 -0400

    Completed TODOs in Main.java and LogoutOutputData.java

--- src/main/java/use_case/logout/LogoutOutputData.java
+++ src/main/java/use_case/logout/LogoutOutputData.java
@@ -9,7 +9,9 @@ public class LogoutOutputData {
     private boolean useCaseFailed;
 
     public LogoutOutputData(String username, boolean useCaseFailed) {
-        // TODO: save the parameters in the instance variables.
+        // save the parameters in the instance variables.
+        this.username = username;
+        this.useCaseFailed = useCaseFailed;
     }
 
     public String getUsername() {
